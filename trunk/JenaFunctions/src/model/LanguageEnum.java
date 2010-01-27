e4c37e10ffe89236d7cffd74863e7115a5ed5d5a
klaudiumihaila <klaudiumihaila@fbc4914e-08d0-11df-9a7a-53bbb444ad82>
2010-01-27 15:42:43 +0000

    completat codurile limbilor din UE

--- trunk/JenaFunctions/src/model/LanguageEnum.java
+++ trunk/JenaFunctions/src/model/LanguageEnum.java
@@ -1,7 +1,27 @@
 package model;
 
 public enum LanguageEnum {
+	BG,
+	CS,
+	DA,
+	DE,
+	EL,
 	EN,
+	ET,
+	ES,
+	FI,
+	FR,
+	GA,
+	HU,
+	IT,
+	LT,
+	LV,
+	MT,
+	NL,
+	PL,
+	PT,
 	RO,
-	FR
+	SK,
+	SL,
+	SV
 }
