4b64825c5202670f8d00fe53e0de3279e61b09e3
klaudiumihaila <klaudiumihaila@fbc4914e-08d0-11df-9a7a-53bbb444ad82>
2010-01-29 22:08:20 +0000

    tiny typo :D

--- trunk/JenaFunctions/src/methods/Test.java
+++ trunk/JenaFunctions/src/methods/Test.java
@@ -2,9 +2,6 @@ package methods;
 
 import com.hp.hpl.jena.query.QuerySolution;
 import com.hp.hpl.jena.query.ResultSet;
-import com.hp.hpl.jena.query.ResultSetFormatter;
-import com.hp.hpl.jena.rdf.model.RDFNode;
-import com.hp.hpl.jena.sparql.engine.binding.Binding;
 
 import utils.Globals;
 import model.Concept;
