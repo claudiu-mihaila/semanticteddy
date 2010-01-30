842bc2509839de62ab40c2c3259156a668ec9cd4
caprice.1612 <caprice.1612@fbc4914e-08d0-11df-9a7a-53bbb444ad82>
2010-01-30 00:29:41 +0000

    added build files

--- teddy/src/adapters/MainPageAdapter.java
+++ teddy/src/adapters/MainPageAdapter.java
@@ -1,8 +1,9 @@
 package adapters;
 
 import java.io.Serializable;
-import java.util.Arrays;
 import java.util.List;
+
+import model.Concept;
 
 import org.richfaces.model.TreeNode;
 import org.richfaces.model.TreeNodeImpl;
@@ -16,34 +17,52 @@ public class MainPageAdapter implements Serializable {
 	 */
 	private static final long serialVersionUID = 5284478227777129030L;
 
-	private TreeNode<String> treeData;
+	private ConceptAdapter conceptAdapter;
+		
+	private TreeNode<Concept> treeData;
+	
+	public MainPageAdapter() {
+		this.conceptAdapter = new ConceptAdapter(this);
+	}
+	
+	public ConceptAdapter getConceptAdapter() {
+		return conceptAdapter;
+	}
 
-	public void setTreeData(TreeNode<String> treeData) {
+	public void setTreeData(TreeNode<Concept> treeData) {
 		this.treeData = treeData;
 	}
 
-	public TreeNode<String> getTreeData() {
+	public TreeNode<Concept> getTreeData() {
 		if (this.treeData == null)
 			this.buildTree();
-		return this.treeData;	}
+		return this.treeData;	
+	}
 
 	private void buildTree() {
-		List<String> roots = Arrays.asList("1","2","3");
-		treeData = new TreeNodeImpl<String>();
-		for (String root: roots){
-			TreeNode<String> uiNode = new TreeNodeImpl<String>();
+		List<Concept> roots = null; //new LinkedList (all root concepts)
+		treeData = new TreeNodeImpl<Concept>();
+		for (Concept root: roots){
+			TreeNode<Concept> uiNode = new TreeNodeImpl<Concept>();
 			uiNode.setData(root);
 			treeData.addChild(root, uiNode);
 			this.createChildren(root, uiNode);
 		}
 	}
 	
-	public void createChildren(String node, TreeNode<String> uiNode) {
-		TreeNode<String> uiChildNode;
-		String childNode = "content";
-		uiChildNode = new TreeNodeImpl<String>();
-		uiChildNode.setData(childNode);
-		uiNode.addChild(childNode, uiChildNode);
-	}	
+	public void createChildren(Concept node, TreeNode<Concept> uiNode) {
+		TreeNode<Concept> uiChildNode;
+		
+		for (Concept c : node.getChildren()) {
+			uiChildNode = new TreeNodeImpl<Concept>();
+			uiChildNode.setData(c);
+			uiNode.addChild(c.getUUID(), uiChildNode);
+			if (null!=c.getChildren() || 0 != c.getChildren().size()){//is leaf
+				this.createChildren(c, uiChildNode);
+			}
+		}
+	}
+
+
 	
 }
