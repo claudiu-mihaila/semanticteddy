191275ad2e351bd63fc84b2a74eda5ee2fc99aa4
klaudiumihaila <klaudiumihaila@fbc4914e-08d0-11df-9a7a-53bbb444ad82>
2010-02-01 01:38:50 +0000

    cocktails in test.java

--- JenaFunctions/src/methods/Test.java
+++ JenaFunctions/src/methods/Test.java
@@ -1,10 +1,5 @@
 package methods;
 
-import com.hp.hpl.jena.rdf.model.Model;
-import com.hp.hpl.jena.rdf.model.Resource;
-import com.hp.hpl.jena.sparql.vocabulary.FOAF;
-import com.hp.hpl.jena.tdb.TDBFactory;
-
 import model.Concept;
 import utils.User;
 
@@ -12,60 +7,60 @@ public class Test {
 
 	public static void main(String[] args) {
 		try {
-		 ThesaurJavaMethods tools = new ThesaurJavaMethods(new User("SimSim", "Sim"), "");
+
+		 ThesaurJavaMethods tools = new ThesaurJavaMethods(new User("SimSim", "Sim"), "E:\\My Folder\\Master\\WADe\\Teddy\\Tests");
+	//	 Model myModel = TDBFactory.createModel("C:\\MyTEstFolder2222");
+	//		Resource myR = myModel.createResource("http://testUri/sim");
+	//		myR.addProperty(FOAF.accountName, "xulescu");
+
+//		 ThesaurJavaMethods tools = new ThesaurJavaMethods(new User("SimSim", "Sim"), "");
+
 			
-		 Concept rootConcept = tools.addRootConcept("MyRoot");
-    	 tools.addDefinition(rootConcept, "def1", "RO");
-    	 tools.addDefinition(rootConcept, "def2", "RO");
-    	 tools.addDefinition(rootConcept, "def3", "EN");
+		 Concept rootConcept = tools.addRootConcept("Cocktails");
+    	 tools.addDefinition(rootConcept, "un stil de bãuturã amestecatã", "RO");
+    	 tools.addDefinition(rootConcept, "a style of mixed drink", "EN");
 		 
-		 Concept child1 = tools.addChildConcept(rootConcept, "Child1");
-		 tools.addAltLabel(child1, "Irina", "RO");
-		 tools.addAltLabel(child1, "Maria", "RO");
-		 tools.addAltLabel(child1, "Irene", "EN");
+		 Concept child1 = tools.addChildConcept(rootConcept, "Pre-dinner");
+		 tools.addAltLabel(child1, "Inainte de masa", "RO");
+		 tools.addAltLabel(child1, "Before dinner", "EN");
 		 
-		 tools.addLatitude(rootConcept, "-55.2355542");
-		 tools.addLongitude(rootConcept, "44.2359957");
-		 tools.editLatitude(rootConcept, "55.2355542");
-		 tools.editLongitude(rootConcept, "-44.2359957");
+		 Concept child2 = tools.addChildConcept(rootConcept, "Long drink");
+		 tools.addDefinition(child2, "Alcohol with a large quantity of juice", "EN");
 		 
-		 Concept child2 = tools.addChildConcept(rootConcept, "Child2");
-		 tools.addDefinition(child2, "Boy", "EN");
-		 tools.addPrefLabel(child2, "Ana", "DE");
+		 Concept child3 = tools.addChildConcept(rootConcept, "After-dinner");
 		 
-		 Concept child3 = tools.addChildConcept(rootConcept, "Child3");
-		 
-		 Concept subChild1 = tools.addChildConcept(child3, "sub3_1");
-		 Concept subChild2 = tools.addChildConcept(child2, "sub3_2");
-		 tools.linkParentConcept(subChild1, rootConcept);
-		 tools.linkRelatedConcept(subChild2, child2);
-		 tools.linkRelatedConcept(subChild2, child3);
-		 
-		 Concept child4 = tools.addChildConcept(subChild2, "Child 4");
-		 tools.addPrefLabel(child4, "Copil 4", "EN");
-		 tools.addAltLabel(child4, "copilas", "RO");
-		 tools.addDefinition(child4, "definit", "FR");
-		 tools.addLatitude(child4, "23.555");
-		 tools.addLongitude(child4, "-55");		 
+		 Concept subChild1 = tools.addChildConcept(child3, "White Russian");
+		 Concept subChild2 = tools.addChildConcept(child2, "Tequila Sunsrise");
+		 Concept subChild3 = tools.addChildConcept(child2, "Tequila Sunset");
+
+		 tools.linkRelatedConcept(subChild2, subChild3);
+
+		 tools.addLatitude(subChild2, "33.448333");
+		 tools.addLongitude(subChild2, "-112.073889");		 
 		 
 	//	 tools.printAsObject(rootConcept);
 		 
-		 tools.removeChildConcept(child3, subChild1);
-		 tools.removeDefinition(rootConcept, "def1", "RO");
-		 tools.editDefinition(rootConcept, "def2", "definitie", "RO");
-		 tools.editPrefLabel(child2, "Child2", "Ioan", "RO");
-		 tools.addPrefLabel(child2, "John", "EN");
-		 tools.removePrefLabel(child2, "Ana", "DE");
-		 tools.removeRelatedConcept(child2, subChild2);
-		 tools.editAltLabel(child1, "Irene", "Mary", "EN");
-		 tools.removeAltLabel(child1, "Irina", "RO");
-		 tools.deleteConcept(child4);
+//		 tools.removeChildConcept(child3, subChild1);
+//		 tools.removeDefinition(rootConcept, "def1", "RO");
+//		 tools.editDefinition(rootConcept, "def2", "definitie", "RO");
+//		 tools.editPrefLabel(child2, "Child2", "Ioan", "RO");
+//		 tools.addPrefLabel(child2, "John", "EN");
+//		 tools.removePrefLabel(child2, "Ana", "DE");
+//		 tools.removeRelatedConcept(child2, subChild2);
+//		 tools.editAltLabel(child1, "Irene", "Mary", "EN");
+//		 tools.removeAltLabel(child1, "Irina", "RO");
+//		 tools.deleteConcept(child4);
 		 
 		// tools.printAsObject(rootConcept);
 		
-		 tools.getRdfModel().exportXML("D:\\MyTestFolderr\\sdasdad\\XMLTry.xml");
-		 tools.getRdfModel().exportTurtle(null);
-		 tools.closeProject();
+		 tools.getRdfModel().getRdfModel().write(System.out, "TURTLE");
+//		 tools.getRdfModel().exportXML("E:/My Folder/Master/WADe/Teddy/Tests/XMLTry.xml");
+		// tools.closeProject();
+
+//		 tools.getRdfModel().exportXML("D:\\MyTestFolderr\\sdasdad\\XMLTry.xml");
+//		 tools.getRdfModel().exportTurtle(null);
+//		 tools.closeProject();
+
 		
 	//	 SampleQueries.SampleQuery1(tools.getRdfModel().getRdfModel());
 
